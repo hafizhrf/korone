4b183208a7027578332602f90f5e75967a8f4f1c
hafizhrf <hafizhsenpai30@gmail.com>
2020-04-20 15:02:23 +0700

    update nh

--- commands/nh.js
+++ commands/nh.js
@@ -10,32 +10,28 @@ module.exports = {
     if(args.length < 1){
       msg.channel.send('Silahkan tulis dojin yang diinginkan / ketik `k!nh random` untuk menampilkan random dojon');
     }else{
-      api.search(args.join(' ')).then(res => {
-        console.log(res);
-      }).catch(err => {
-        console.log(err);
-      })
-      // axios.get(URL).then(res => {
-      //   if (res.data.results.length < 1) {
-      //     msg.channel.send('Manga tidak ditemukan');
-      //   }else{
-      //     const embed = new MessageEmbed()
-      //     .setColor(0xff0000)
-      //     .setDescription(res.data.results[0].synopsis)
-      //     .setAuthor(res.data.results[0].title , res.data.results[0].image_url)
-      //     .setFooter("Copyright Myanimelist", "https://cdn.myanimelist.net/images/faviconv5.ico")
-      //     .setImage(res.data.results[0].image_url)
-      //     .addField("Type", res.data.results[0].type, false)
-      //     .addField("Volumes", res.data.results[0].volumes, false)
-      //     .addField("Chapters", res.data.results[0].chapters, false)
-      //     .addField("Score", res.data.results[0].score, false)
-      //     .addField("Members", formatNumber(res.data.results[0].members), false)
-      //     .addField("Publishing", res.data.results[0].publishing ? 'Yes' : 'No', false)
-      //     msg.channel.send(embed)
-      //   }
-      // }).catch(err => {
-      //   msg.channel.send('Manga tidak ditemukan');
-      // })
+      if (args.join(' ').length == 6 && !isNaN(args.join(' '))) {
+        console.log(args.join(' '));
+        api.g(args.join(' ')).then(gallery => {
+          const embed = new MessageEmbed()
+          .setColor(0xff0000)
+          .setAuthor(gallery.title.english , gallery.getCover())
+          .setFooter("Copyright Nhentai")
+          msg.channel.send(embed)
+        }).catch(err => {
+          msg.channel.send('Doujin tidak ditemukan');
+        })
+      }else{
+        api.search(args.join(' ')).then(res => {
+          const embed = new MessageEmbed()
+          .setColor(0xff0000)
+          .setAuthor(res.results[0].title , res.results[0].thumbnail.s)
+          .setFooter("Copyright Nhentai")
+          msg.channel.send(embed)
+        }).catch(err => {
+          msg.channel.send('Doujin tidak ditemukan');
+        })
+      }
     }
   },
 };
