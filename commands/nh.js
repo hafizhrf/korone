c91bb56c260de26461496a424694a56a61c113a8
hafizhrf <hafizhsenpai30@gmail.com>
2020-04-20 15:49:18 +0700

    update random doujin

--- commands/nh.js
+++ commands/nh.js
@@ -10,7 +10,42 @@ module.exports = {
     if(args.length < 1){
       msg.channel.send('Silahkan tulis dojin yang diinginkan / ketik `k!nh random` untuk menampilkan random dojon');
     }else{
-      if (args.join(' ').length == 6 && !isNaN(args.join(' '))) {
+      if (args.join(' ') === 'random') {
+        api.random().then(gallery => {
+          const embed = {
+            color: 6101172,
+            footer: {
+              text: "Copyright nhentai.net"
+            },
+            image: {
+              url: gallery.getCover()
+            },
+            author: {
+              name: gallery.title.english,
+              url: `https://nhentai.net/g/${gallery.id}`,
+            },
+            fields: [
+              {
+                name: 'Pages',
+                value: gallery.num_pages,
+                inline: false
+              }
+            ],
+          }
+          let tags = ''
+          gallery.tags.map(res => {
+            tags += `${res.name}, `
+          })
+          embed.fields.push({
+            name: "Tags",
+            value: tags.slice(0,-2),
+            inline: false
+          })
+          msg.channel.send({embed:embed})
+        }).catch(err => {
+          msg.channel.send('Doujin tidak ditemukan');
+        })
+      }else if (args.join(' ').length === 6 && !isNaN(args.join(' '))) {
         api.g(args.join(' ')).then(gallery => {
           const embed = {
             color: 6101172,
