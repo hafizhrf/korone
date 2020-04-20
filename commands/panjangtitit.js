5751a5fbba31a8d99b4bdc5b00ccc4c24a327aed
hafizhrf <hafizhsenpai30@gmail.com>
2020-04-20 22:48:02 +0700

    update panjang

--- commands/panjangtitit.js
+++ commands/panjangtitit.js
@@ -4,9 +4,29 @@ module.exports = {
   execute(msg, args) {
     let id = Math.floor(Math.random() * 100);
     if(args.length < 1){
-      msg.reply("Panjang titidmu adalah ${id}cm");
+      if(id > 80){
+        msg.reply(`Panjang titidmu adalah ${id}cm, gokil aku suka :flushed:`);
+      }else if(id > 60){
+        msg.reply(`Panjang titidmu adalah ${id}cm, boleh lah aku suka :flushed:`);
+      }else if(id > 30){
+        msg.reply(`Panjang titidmu adalah ${id}cm, b aja`);
+      }else{
+
+        msg.reply(`Panjang titidmu adalah ${id}cm, ih pendek`);
+
+      }
     }else{
-      msg.channel.send(`Panjang titid ${args[0]} adalah ${id}cm`);
+      if(id > 80){
+        msg.channel.send(`Panjang titid ${args[0]} adalah ${id}cm, gokil aku suka :flushed:`);
+      }else if(id > 60){
+        msg.channel.send(`Panjang titid ${args[0]} adalah ${id}cm, boleh lah aku suka :flushed:`);
+      }else if(id > 30){
+        msg.channel.send(`Panjang titid ${args[0]} adalah ${id}cm, b aja`);
+      }else{
+
+        msg.channel.send(`Panjang titid ${args[0]} adalah ${id}cm, ih pendek`);
+
+      }
     }
   },
 };
