1cd64850b714c22c0b671a466fdb677fe33a71b1
hafizhrf <hafizhsenpai30@gmail.com>
2020-04-20 16:05:11 +0700

    update command

--- commands/nh.js
+++ commands/nh.js
@@ -2,6 +2,42 @@ require('dotenv').config();
 const { MessageEmbed } = require('discord.js');
 const nHentaiAPI = require('nhentai-api-js');
 let api = new nHentaiAPI();
+const getDoujin(id, msg){
+  api.g(id).then(gallery => {
+    const embed = {
+      color: 6101172,
+      footer: {
+        text: "Copyright nhentai.net"
+      },
+      image: {
+        url: gallery.getCover()
+      },
+      author: {
+        name: gallery.title.english,
+        url: `https://nhentai.net/g/${gallery.id}`,
+      },
+      fields: [
+        {
+          name: 'Pages',
+          value: gallery.num_pages,
+          inline: false
+        }
+      ],
+    }
+    let tags = ''
+    gallery.tags.map(res => {
+      tags += `${res.name}, `
+    })
+    embed.fields.push({
+      name: "Tags",
+      value: tags.slice(0,-2),
+      inline: false
+    })
+    msg.channel.send({embed:embed})
+  }).catch(err => {
+    msg.channel.send('Doujin tidak ditemukan');
+  })
+}
 
 module.exports = {
   name: 'k!nh',
@@ -11,112 +47,13 @@ module.exports = {
       msg.channel.send('Silahkan tulis dojin yang diinginkan / ketik `k!nh random` untuk menampilkan random dojon');
     }else{
       if (args.join(' ') === 'random') {
-        api.random().then(gallery => {
-          const embed = {
-            color: 6101172,
-            footer: {
-              text: "Copyright nhentai.net"
-            },
-            image: {
-              url: gallery.images.cover.t
-            },
-            author: {
-              name: gallery.title.english,
-              url: `https://nhentai.net/g/${gallery.id}`,
-            },
-            fields: [
-              {
-                name: 'Pages',
-                value: gallery.num_pages,
-                inline: false
-              }
-            ],
-          }
-          let tags = ''
-          gallery.tags.map(res => {
-            tags += `${res.name}, `
-          })
-          embed.fields.push({
-            name: "Tags",
-            value: tags.slice(0,-2),
-            inline: false
-          })
-          msg.channel.send({embed:embed})
-        }).catch(err => {
-          console.log(err);
-          msg.channel.send('Doujin tidak ditemukan');
-        })
+        let id = Math.floor(Math.random() * 300000);
+        getDoujin(id, msg)
       }else if (args.join(' ').length === 6 && !isNaN(args.join(' '))) {
-        api.g(args.join(' ')).then(gallery => {
-          const embed = {
-            color: 6101172,
-            footer: {
-              text: "Copyright nhentai.net"
-            },
-            image: {
-              url: gallery.getCover()
-            },
-            author: {
-              name: gallery.title.english,
-              url: `https://nhentai.net/g/${args.join(' ')}`,
-            },
-            fields: [
-              {
-                name: 'Pages',
-                value: gallery.num_pages,
-                inline: false
-              }
-            ],
-          }
-          let tags = ''
-          gallery.tags.map(res => {
-            tags += `${res.name}, `
-          })
-          embed.fields.push({
-            name: "Tags",
-            value: tags.slice(0,-2),
-            inline: false
-          })
-          msg.channel.send({embed:embed})
-        }).catch(err => {
-          msg.channel.send('Doujin tidak ditemukan');
-        })
+        getDoujin(args.join(' '), msg)
       }else{
         api.search(args.join(' ')).then(res => {
-          api.g(res.results[0].id).then(gallery => {
-            const embed = {
-              color: 6101172,
-              footer: {
-                text: "Copyright nhentai.net"
-              },
-              image: {
-                url: gallery.getCover()
-              },
-              author: {
-                name: gallery.title.english,
-                url: `https://nhentai.net/g/${res.results[0].id}`,
-              },
-              fields: [
-                {
-                  name: 'Pages',
-                  value: gallery.num_pages,
-                  inline: false
-                }
-              ],
-            }
-            let tags = ''
-            gallery.tags.map(res => {
-              tags += `${res.name}, `
-            })
-            embed.fields.push({
-              name: "Tags",
-              value: tags.slice(0,-2),
-              inline: false
-            })
-            msg.channel.send({embed:embed})
-          }).catch(err => {
-            msg.channel.send('Doujin tidak ditemukan');
-          })
+          getDoujin(res.results[0].id, msg)
         }).catch(err => {
           msg.channel.send('Doujin tidak ditemukan');
         })
